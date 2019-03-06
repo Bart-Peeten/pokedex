02d59bac984a0e2d3438427accd9db1c1d501be3
Bart-Peeten <bart.peeten@gmail.com>
2019-03-06 22:33:50 +0100

    fix test file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {expect} from "@angular/platform-browser/testing/src/matchers";
+import {describe} from "@angular/core/testing/src/testing_internal";
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
